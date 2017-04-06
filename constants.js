9c3e2c31510a0a2d43f60d6b1155526b5c53c0c4
Gustavo Stor de Aguiar <stor@fb.com>
2017-04-05 21:44:30 -0700

    wip

--- constants.js
+++ constants.js
@@ -48,6 +48,48 @@ C.OJS = [
   'zoj',
 ];
 
+C.JUDGE = {
+  STATUS: {
+    'COMPILING': -4,
+  	'RUNNING': -3,
+  	'LINKING': -2,
+  	'ON_JUDGE_QUEUE': -1,
+  	'PENDING': 0,
+  	'ACCEPTED': 1,
+  	'WRONG_ANSWER': 2,
+  	'TIME_LIMIT': 3,
+  	'COMPILE_ERROR': 4,
+  	'RUNTIME_ERROR': 5,
+  	'MEMORY_LIMIT': 6,
+  	'OUTPUT_LIMIT': 7,
+  	'PRESENTATION_ERROR': 8,
+    'UNKNOWN_ERROR': 9, // DEPRECATED
+    'RESTRICTED_FUNCTION': 10,
+    'INTERNAL_ERROR': 11, // no retry, no penalty
+    'SUBMISSION_ERROR': 12, // retry, no penalty
+  },
+
+  VERDICT: {
+    '-4': 'Compilando...',
+    '-3': 'Executando...',
+    '-2': 'Compilando...',
+    '-1': 'Enviado para Correção',
+    '0': 'Pendendo',
+    '1': 'Aceito',
+    '2': 'Resposta Errada',
+    '3': 'Tempo Limite Excedido',
+    '4': 'Erro de Compilação',
+    '5': 'Erro durante Execução',
+    '6': 'Limite de Memória Excedido',
+    '7': 'Limite de Escrita Excedido',
+    '8': 'Erro de Apresentação',
+    '9': 'Erro Desconhecido', // DEPRECATED
+    '10': 'Uso de função restrita',
+    '11': 'Erro Interno',
+    '12': 'Erro de submissão',
+  },
+};
+
 C.SITE_VARS = {
   ACCESS: {
     MEMBER: 0,
