49d404b5fe6bb527cd98cbbc2e135e3b22b9b4db
Gustavo Stor <gustavostor@gmail.com>
2022-04-23 02:11:57 -0400

    Last changes before launch

--- models/post.js
+++ models/post.js
@@ -1,19 +1,21 @@
-"use strict";
+require(`./user`);
 
-require("./user");
+const mongoose = require(`mongoose`);
+const Joi = require(`joi`);
+const CollectionCounter = require(`./collection_counter`);
+const aws = require(`aws-sdk`);
+const { CONN } = require(`../constants`);
 
-const mongoose = require("mongoose");
-const CollectionCounter = require("./collection_counter");
-const Joi = require("joi");
+const S3 = new aws.S3({ params: { Bucket: CONN.GET_S3_BUCKET() } });
 
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const { ObjectId } = mongoose.Schema.Types;
 
-let schema = mongoose.Schema(
+const schema = mongoose.Schema(
   {
     _id: { type: Number, min: 1 },
     author: {
       type: ObjectId,
-      ref: "User",
+      ref: `User`,
     },
     title: String,
     body: String,
@@ -36,17 +38,31 @@ schema.index({ id: 1 });
 schema.index({ author: 1, createdAt: -1 });
 schema.index({ page: 1, createdAt: -1 });
 
-schema.statics.validate = function (obj) {
-  return Joi.object({
+schema.statics.validate = (obj) =>
+  Joi.object({
     title: Joi.string().min(1).max(100).required(),
     body: Joi.string().min(1).max(2000).required(),
     author: Joi.string().min(1).max(100).required(),
     blog: Joi.string().min(1).max(100).optional(),
   }).validate(obj);
-};
 
-schema.pre("save", async function () {
-  await CollectionCounter.setIncrementalId(this, "Post");
+const C = require(`../constants`);
+schema.pre(`save`, async function preSave() {
+  await CollectionCounter.setIncrementalId(this, `Post`);
+  const path = `assets/blog/${this._id}.html`;
+  console.log(this.body);
+  S3.upload(
+    {
+      Key: path,
+      Body: this.body,
+      ACL: `public-read`,
+      CacheControl: `max-age=1209600`,
+    },
+    (err, details) => {
+      console.log(details);
+    }
+  );
+  this.body = `${C.STATIC_ASSETS_DOMAIN}/blog/${this._id}.html`;
 });
 
-module.exports = mongoose.model("Post", schema);
+module.exports = mongoose.model(`Post`, schema);
